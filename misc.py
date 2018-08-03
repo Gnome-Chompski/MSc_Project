e915bac9df409b8557db404cb9b6b69a616e139d
Gnome-Chompski <kIjygg38@gmail.com>
2018-08-03 14:15:48 +0100

    Private repo upload

--- misc.py
+++ misc.py
@@ -1,10 +1,17 @@
-testdict = {'apples':'fruit', 'dictionary':{'cabbage':'iceberg', 'dictionar':{'deep':'deacons'}}, 'oranges': 'fruit'}
+import tweepy, csv, numpy, json
+import pandas as pd
+from pprint import pprint
 
-def iterdict(dict1, tab):
-    for key in dict1:
-        if type(dict1[key]) is dict:
-            tab += "\t"
-            print(tab + str(key) + " : ")
-            iterdict(dict1[key], tab)
-        else:
-            print(tab + str(key) + " : " + dict1[key])
+with open("handlesTwitter.txt", "r", encoding="utf-8") as users:
+    for line in users:
+        FILENAME = "userTweets/" + line.strip() + ".csv"
+        print("Processing " + line.strip())
+        with open(FILENAME, "r", encoding="utf-8") as file:
+            data = file.readlines()
+
+        line1 = data[0].partition("retweeted")
+        corrected = [line1[0] + line1[1] + "\n", line1[2]]
+        corrected.extend(data[1:])
+
+        with open(FILENAME, "w", encoding="utf-8") as file:
+            file.writelines(corrected)
--- twittertest.py
+++ twittertest.py
@@ -1,20 +1,30 @@
-import tweepy
+import tweepy, csv
 
-CONSUMER_KEY = "92TERgNQ6JPXybIvaocgVQvhG"
+CONSUMER_KEY = "2iZwoiqZCyCeckD6RONU7yl79"
 ACCESS_TOKEN = ""
 CONSUMER_SECRET = ""
 ACCESS_SECRET = ""
 
 with open("consumersecret.key") as f:
     CONSUMER_SECRET = f.read().strip()
+f.close()
 
 with open("accesskey.key") as f:
     ACCESS_TOKEN = f.readline().strip()
     ACCESS_SECRET = f.readline().strip()
+f.close()
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
 
 def displayUserInformation(username):
     user = api.get_user(username)
@@ -24,8 +34,7 @@ def displayUserInformation(username):
         print(userdict[key])
 
 def displayUserTimeline(username, count=25, simple=False):
-    timeline = api.user_timeline(username, count = count)
-    for tweet in timeline:
+    for tweet in tweepy.Cursor(api.user_timeline, count=count, id=username).items():
         tweetdict = tweet.__dict__['_json']
         if simple:
             print("TweetID " + tweetdict['id_str'])
@@ -35,13 +44,14 @@ def displayUserTimeline(username, count=25, simple=False):
             for key in tweetdict:
                 print(key + " : ", end="")
                 print(tweetdict[key])
-
+        keys = tweet.__dict__.keys()
+        print(keys)
 
 def get_userinfo(name):
     user = api.get_user(name)
     user_info = [name.encode('utf-8'), user.name.encode('utf-8'), user.description.encode('utf-8'), user.followers_count, user.friends_count, user.created_at,	user.location.encode('utf-8')]
     print(user_info)
 
-#displayUserTimeline('PredaDante', simple=True)
-get_userinfo("PredaDante")
-displayUserInformation("VisitScotland")
+#displayUserTimeline('johnny_oliveira', count=10, simple=True)
+#get_userinfo("PredaDante")
+displayUserInformation("PredaDante")
